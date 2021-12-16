833087e4d3497bef54d506793f20576e5ffd06e9
Charles Roth <charlieroth4@icloud.com>
2021-12-16 17:59:13 +0100

    day03 part2 complete

--- 2021/day03/part02/main.go
+++ 2021/day03/part02/main.go
@@ -5,40 +5,140 @@ import (
   "os"
   "bufio"
   "log"
+  "strconv"
   "math"
 )
 
-func readFile(path string) map[string]bool {
+func readFile(path string) []string {
   file, err := os.Open(path)
   if err != nil {
     log.Fatal(err)
   }
   defer file.Close()
 
-  m := make(map[string]bool)
+  var nums []string
   scanner := bufio.NewScanner(file)
   for scanner.Scan() {
     line := scanner.Text()
-    m[line] = true
+    nums = append(nums, line)
   }
 
-  return m
+  return nums
 }
 
-func mostCommonBitByPosition(position int, numMap map[string]bool) {
+func createBinary(num string) []int {
+  binaryArr := make([]int, len(num))
 
+  for i, bitC := range num {
+    bitStr := string(bitC)
+    bit, err := strconv.Atoi(bitStr)
+    if err != nil {
+      log.Fatal(err)
+    }
+    binaryArr[i] = bit;
+  }
+
+  return binaryArr;
+}
+
+func binaryArrToDecimal(binary []int) float64 {
+  pow := float64(len(binary) - 1)
+  decimal := 0.0
+  for _, bit := range binary {
+    var fBit = float64(bit)
+    decimal += fBit * math.Pow(2, pow)
+    if pow >= 1.0 { pow -= 1.0 }
+  }
+
+  return decimal
+}
+
+func mostCommonBitByIndex(nums []string, index int) byte {
+  oneCount := 0
+  zeroCount := 0
+  for _, num := range nums {
+    if num[index] == '1' {
+      oneCount += 1
+    } else {
+      zeroCount += 1
+    }
+  }
+
+  var bitToReturn byte
+  if oneCount > zeroCount { bitToReturn = '1' }
+  if zeroCount > oneCount { bitToReturn = '0' }
+  if zeroCount == oneCount { bitToReturn = '1' }
+  return bitToReturn
+}
+
+func leastCommonBitByIndex(nums []string, index int) byte {
+  oneCount := 0
+  zeroCount := 0
+  for _, num := range nums {
+    if num[index] == '1' {
+      oneCount += 1
+    } else {
+      zeroCount += 1
+    }
+  }
+
+  var bitToReturn byte
+  if oneCount < zeroCount { bitToReturn = '1' }
+  if zeroCount < oneCount { bitToReturn = '0' }
+  if zeroCount == oneCount { bitToReturn = '0' }
+  return bitToReturn
+}
+
+func removeByBit(nums []string, index int, bit byte) []string {
+  var newNums []string
+  for _, num := range nums {
+    // If first character is the same as the bit
+    if num[index] == bit {
+      newNums = append(newNums, num)
+    }
+  }
+
+  return newNums
+}
+
+func getOxygenGeneratorRating() float64 {
+  nums := readFile("input.txt")
+  index := 0
+  alive := len(nums)
+
+  for alive > 1 {
+    bit := mostCommonBitByIndex(nums, index)
+    nums = removeByBit(nums, index, bit)
+    alive = len(nums)
+    index += 1
+  }
+  
+  binaryArr := createBinary(nums[0])
+  fmt.Println(binaryArr)
+  decimal := binaryArrToDecimal(binaryArr)
+  return decimal
+}
+
+func getCO2ScrubberRating() float64 {
+  nums := readFile("input.txt")
+  index := 0
+  alive := len(nums)
+
+  for alive > 1 {
+    bit := leastCommonBitByIndex(nums, index)
+    nums = removeByBit(nums, index, bit)
+    alive = len(nums)
+    index += 1
+  }
+  
+  binaryArr := createBinary(nums[0])
+  fmt.Println(binaryArr)
+  decimal := binaryArrToDecimal(binaryArr)
+  return decimal
 }
 
 func main() {
-  numMap := readFile("example.txt")
-
-  currPosition := 0
-  alive := len(numMap)
-
-  for alive > 1 {
-    // TODO: Find most common bit in current position evaluating
-    // TODO: Remove numbers without most common bit in current position
-    // TODO: Update alive count
-  }
+  oxygenGeneratorRating := getOxygenGeneratorRating()
+  co2ScrubberRating := getCO2ScrubberRating()
+  fmt.Println("result: ", co2ScrubberRating * oxygenGeneratorRating)
 }
-
